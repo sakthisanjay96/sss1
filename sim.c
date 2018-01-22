aa02c0105c6a0870718c92e1dae76ae7722bec37
sakthisanjay96 <30748370+sakthisanjay96@users.noreply.github.com>
2018-01-22 16:23:39 +0530

    Create sim.c

---
+++ sim.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a,b,i,s;
+printf("\nenter the first number");
+scanf("%d",&a);
+printf("\nenter the final number");
+scanf("%d",&b);
+for(i=a;i<=b;i++)
+{
+s=i%2;
+}
+if(s==0)
+{
+printf("\nthe even number between %d and %d \n: a,b");
+printf("\n%d",i);
+}
+}
