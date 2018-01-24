3142c8df72031d45d74ff469153edd566f3b4cbd
sakthisanjay96 <30748370+sakthisanjay96@users.noreply.github.com>
2018-01-24 15:59:41 +0530

    Create mul.c

---
+++ mul.c
@@ -0,0 +1,16 @@
+# include<stdio.h> 
+int main() 
+{ 
+ int i,n,s,p; 
+ printf("Enter the limit : ") ; 
+ scanf("%d",&n) ; 
+ printf("\nEnter the number : ") ; 
+ scanf("%d",&s) ; 
+   printf("\nThe numbers divisible by %d are :\n\n",p) ; 
+ for(i = 1 ; i <= n ; i++) 
+
+  if(i % p == 0) 
+    
+   printf("%d\t",i) ; 
+ return 0;
+}
