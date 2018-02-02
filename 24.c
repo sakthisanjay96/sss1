8713a638663c3224143793092670e9bf78bed8e4
sakthisanjay96 <30748370+sakthisanjay96@users.noreply.github.com>
2018-02-02 16:17:31 +0530

    Update 24.c

--- 24.c
+++ 24.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 void main()
 {
-	int i,j,n,a[n];
-	scanf("%d",&n);
-	for(int i=0;i<n;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	for(i=0;i<n;i++)
-	{
-		for(j=i+1;j<n;j++)
-		{
-			if(a[i]>a[j])
-			{
-				int t;
-				t=a[i];
-				a[i]=a[j];
-				a[j]=t;
-			}
-		}
-	}
-	for(i=0;i<n;i++)
-	{
-	printf("%d",a[i]);
-	}
+int i,j,n,a[n];
+scanf("%d",&n);
+for(int i=0;i<n;i++)
+{	scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+int temp;
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+for(i=0;i<n;i++)
+{printf("%d",a[i]);
+}
 }
