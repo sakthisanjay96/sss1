422637703f0d9b5b3f84e2cc6ac7479dd6d0120c
sakthisanjay96 <30748370+sakthisanjay96@users.noreply.github.com>
2018-02-02 15:43:58 +0530

    Create 21.c

---
+++ 21.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a, b,c ,d,e;
+    printf("enter the number a:");
+    scanf("%d",&a);
+    printf("\nenter the number b:");
+    scanf("%d",&b);
+    printf("\nenter the number c:");
+    scanf("%d",&c);
+    d=b+c;
+    e=a*d;
+    printf ("%d",e);
+    return 0;
+}
