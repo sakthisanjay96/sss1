d24699a80115cba561e4c212766ffd0ea7d4ab99
sakthisanjay96 <30748370+sakthisanjay96@users.noreply.github.com>
2018-02-05 16:06:42 +0530

    Update 30.c

--- 30.c
+++ 30.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main()
 {
-int a,b,c,d,e,f;
+int a,b,c,d,p,q;
 printf("give the 1st time");
 scanf("%d %d",&a,&b);
 printf("\ngive the 2nd time");
 scanf("%d %d",&c,&d);
-e=a-c;
-f=b-d;
-printf("\n%d %d",e,f);
+p=a-c;
+q=b-d;
+printf("\n%d %d",p,q);
 return 0;
 }
