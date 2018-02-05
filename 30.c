fd99104ab18e3e5a53188fc6e125cffb9cfa7e93
sakthisanjay96 <30748370+sakthisanjay96@users.noreply.github.com>
2018-02-05 16:05:56 +0530

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int a,b,c,d,e,f;
+printf("give the 1st time");
+scanf("%d %d",&a,&b);
+printf("\ngive the 2nd time");
+scanf("%d %d",&c,&d);
+e=a-c;
+f=b-d;
+printf("\n%d %d",e,f);
+return 0;
+}
