3c7cb135aecf048a5aa74bed2d55d293a5325990
sakthisanjay96 <30748370+sakthisanjay96@users.noreply.github.com>
2018-02-05 15:51:24 +0530

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() 
+{
+    int a,b,c,n,i;
+    printf("enter the three number:");
+    scanf("%d%d%d",&a,&b,&c);
+    for(i=0;i<n;i++)
+{
+scanf("%d",&a);
+}
+for(i=0;i<n;i++)
+{
+printf("%d\t%d\n",a,i);
+}
+return 0;
+}
