43f2b075a529296dc60259a83ecf4574cf9ade02
sakthi s sanjay <30748370+sakthisanjay96@users.noreply.github.com>
2018-02-19 16:28:56 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int p,q,r,s,t,x;
+	printf("enter the numbers:");
+	scanf("%d%d%d%d%d",&p,&q,&r,&s,&t);
+	x=(p+q+r+s+t)/5;
+	printf("the number is %d",x);
+	return 0;
+}
