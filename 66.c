c6ff8dcb3227b04dec5d5ca50bafcfdbff72649b
sakthi s sanjay <30748370+sakthisanjay96@users.noreply.github.com>
2018-03-02 16:13:21 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int p;
+printf("enter the number:");
+scanf("%i",&p);
+if(p%p==0&&p%1==0)
+{
+printf("prime");
+}
+else
+{
+printf("is not prime");
+}
+return 0;
+}
