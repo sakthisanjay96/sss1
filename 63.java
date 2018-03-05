0b5a5a6416a05d4e2b20fb9f850a0ae72170ba36
sakthi s sanjay <30748370+sakthisanjay96@users.noreply.github.com>
2018-03-05 15:37:11 +0530

    Create 63.java

---
+++ 63.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+public class Prgm63
+{
+	public static void main(String s[])
+	{
+		int d=0,i;
+		String n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextLine();
+		String aa=n.split("\\sc+");
+		for(i=0;i<aa.length;i++)
+		{
+			d++;
+		}
+	}
+}
