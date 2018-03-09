49e2da1156a492c843e9b9332cdca2b2c9e88daa
sakthi s sanjay <30748370+sakthisanjay96@users.noreply.github.com>
2018-03-09 15:40:46 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+float a,b,c;
+printf("enter the length: \n");
+scanf("%f",&a);
+printf("enter the breadth \n");
+scanf("%f",&b);
+c=a*b;
+printf("area of farm %f",c);
+return 0;
+}
