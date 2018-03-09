6cce71e7826898ba6d5e39925e59cf4a713afc7d
sakthi s sanjay <30748370+sakthisanjay96@users.noreply.github.com>
2018-03-09 15:46:52 +0530

    Create 83.c

---
+++ 83.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int p,q,r,s;
+printf("enter the a and b values:");
+scanf("%d%d",&p,&q);
+r=p/q;
+printf("%d \n",r);
+printf("enter the a and b values:");
+scanf("%d%d",&p,&q);
+s=p%q;
+printf("%d \n",s);
+return 0;
+}
