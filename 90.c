b0458f7c1b151d38e4a495c8d2ea18ec1901d5b7
sakthi s sanjay <30748370+sakthisanjay96@users.noreply.github.com>
2018-03-12 15:46:13 +0530

    Create 90.c

---
+++ 90.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char p[10],i;
+printf("enter the string");
+scanf("%s",&p[i]);
+for(i=0;p[i]!='\0';i++)
+{
+if(p[i]>='0'&&p[i]<='9')
+{
+printf(" %c",p[i]);
+}
+}
+return 0;
+}
